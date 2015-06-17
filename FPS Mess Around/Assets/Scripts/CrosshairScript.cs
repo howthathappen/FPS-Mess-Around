08de721c6eba2d0604436144bf501f6e14ff81b2
Steven B <Stevjlk@hotmail.com>
2015-06-17 08:33:31 -0400

    Added a crosshair and made sure the gun would always shoot at the exact center of the screen.

---
+++ FPS Mess Around/Assets/Scripts/CrosshairScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrosshairScript : MonoBehaviour {
+
+	public Texture2D crosshairTexture;
+	public float crosshairScale = 1f;
+	
+	private void OnGUI ()
+	{
+		if(Time.timeScale != 0) //can set timescale to 0 to pause the game I guess
+		{
+			if(crosshairTexture != null) //if we have a texture to use
+			{
+				//draw the texture in the center of the screen
+				GUI.DrawTexture(new Rect((Screen.width - crosshairTexture.width*crosshairScale)/2, (Screen.height - crosshairTexture.height*crosshairScale)/2, //set the x, y coordinates
+								crosshairTexture.width*crosshairScale, crosshairTexture.height*crosshairScale), //set the width and height of the rectangle
+								crosshairTexture); //tell unity which texture to use
+			}
+			else
+			{
+				Debug.Log("No crosshair texture set in the inspector");
+			}
+		}
+	}
+}
--- FPS Mess Around/Assets/Scripts/LaserScript.cs
+++ FPS Mess Around/Assets/Scripts/LaserScript.cs
@@ -5,14 +5,14 @@ public class LaserScript : MonoBehaviour {
 
 	private LineRenderer line;
 	private GUIStyle guiStyle = new GUIStyle();
-	private Light light;
+	private Light _light;
 	
 	void Start () 
 	{
 		line = this.gameObject.GetComponent<LineRenderer>();
 		line.enabled = false;
 		
-		light = this.gameObject.GetComponent<Light>();
+		_light = this.gameObject.GetComponent<Light>();
 		
 		Cursor.lockState = CursorLockMode.Locked; //locks and hides the cursor
 		Cursor.visible = false;
@@ -30,35 +30,60 @@ public class LaserScript : MonoBehaviour {
 	private IEnumerator FireLaser ()
 	{
 		line.enabled = true;
-		light.enabled = true;
+		_light.enabled = true;
 		
 		while(Input.GetButton("Fire1"))
 		{
-			line.material.mainTextureOffset = new Vector2(0, Time.time);
+			line.material.mainTextureOffset = new Vector2(0, Time.time); //doesn't really do anything with a solid color
 		
-			Ray ray = new Ray(transform.position, transform.forward);
-			RaycastHit hit;
+			//Ray ray = new Ray(transform.position, transform.forward); //their code
+			//RaycastHit hit; //their code
 			
-			line.SetPosition(0, ray.origin);
+			//line.SetPosition(0, ray.origin); //their code
 			
-			if(Physics.Raycast(ray, out hit,100))
-			{
-				line.SetPosition(1, hit.point);
-				if(hit.rigidbody)//if our target has a rigidbody
-				{
-					hit.rigidbody.AddForceAtPosition(transform.forward * 5, hit.point);
-				}
-			}
-			else
-			{
-				line.SetPosition(1, ray.GetPoint(100));
-			}
+			line.SetPosition(0, transform.position); //my code
+			ShootAtCenterOfScreen(); //my code
+			
+			//their code
+//			if(Physics.Raycast(ray, out hit, 100))
+//			{
+//				line.SetPosition(1, hit.point);
+//				if(hit.rigidbody)//if our target has a rigidbody
+//				{
+//					hit.rigidbody.AddForceAtPosition(transform.forward * 5, hit.point);
+//				}
+//			}
+//			else
+//			{
+//				line.SetPosition(1, ray.GetPoint(100));
+//			}
 			
 			yield return null;
 		}
 		
 		line.enabled = false;
-		light.enabled = false;
+		_light.enabled = false;
+	}
+	
+	//my functions
+	private void ShootAtCenterOfScreen ()
+	{
+		Vector2 screenCenter = new Vector2(Screen.width/2, Screen.height/2);
+		Ray myRay = Camera.main.ScreenPointToRay(screenCenter);
+		RaycastHit hit2;
+		
+		if(Physics.Raycast(myRay, out hit2, 100))
+		{
+			line.SetPosition(1, hit2.point);
+			if(hit2.rigidbody)
+			{
+				hit2.rigidbody.AddForceAtPosition(transform.forward * 5, hit2.point);
+			}
+		}
+		else
+		{
+			line.SetPosition(1, myRay.GetPoint(100));
+		}
 	}
 	
 	private void OnGUI ()
