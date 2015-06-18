ef6373497dbe11c384b40dbe28f5deb8fc9d030d
Steven B <Stevjlk@hotmail.com>
2015-06-18 01:06:16 -0400

    Weapon Swapping added.

---
+++ FPS Mess Around/Assets/Scripts/WeaponSwappingScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WeaponSwappingScript : MonoBehaviour {
+
+	private List<GameObject> gunList = new List<GameObject>();
+	private int currentWeaponIndex = 0;
+	
+	void Start ()
+	{
+		for(int i = 0; i < this.gameObject.transform.childCount; i++)
+		{
+			gunList.Add(this.gameObject.transform.GetChild(i).gameObject);
+		}
+	}
+	
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Q))
+		{
+			//this.gameObject.transform.GetChild(currentWeaponIndex).gameObject.SetActive(false);
+			gunList[currentWeaponIndex].SetActive(false);
+			currentWeaponIndex++;
+			if(currentWeaponIndex == gunList.Count)
+			{
+				currentWeaponIndex = 0;
+			}
+			gunList[currentWeaponIndex].SetActive(true);
+		}
+	}
+}
